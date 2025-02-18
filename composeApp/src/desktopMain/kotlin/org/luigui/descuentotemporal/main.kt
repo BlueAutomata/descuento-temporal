39c58bf052ffc6790efa1ed029bd6f75fc87dbd0
Guillermo Luigui Nieto <guillermo.luigui.nieto@gmail.com>
2025-02-18 13:15:58 -0500

    set fixed screen size

--- composeApp/src/desktopMain/kotlin/org/luigui/descuentotemporal/main.kt
+++ composeApp/src/desktopMain/kotlin/org/luigui/descuentotemporal/main.kt
@@ -1,12 +1,19 @@
 package org.luigui.descuentotemporal
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    // Set a fixed window size
+    val windowState = rememberWindowState(width = 1024.dp, height = 768.dp)
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "DescuentoTemporal",
+        state = windowState,
+        resizable = false
     ) {
         App()
     }
